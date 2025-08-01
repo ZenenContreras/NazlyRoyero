608f59ec4785c80341180353fbf04e026d45fd94
Zenen Contreras Royero <zenencontreras1@gmail.com>
2025-07-31 19:18:20 -0500

    fix: corregir la estructura de la cuadrícula en MethodPage para mejorar la disposición de los pilares

--- src/components/pages/MethodPage.tsx
+++ src/components/pages/MethodPage.tsx
@@ -243,7 +243,7 @@ const MethodPage = () => {
             </h2>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-12">
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-6 md:gap-8 lg:gap-12">
             {pillars.map((pillar, index) => {
               return (
                 <div
